3e1beffdd063fcf7dd06e663d818e88e04f27263
Matt Graham <grahammatt@live.com>
2018-02-11 13:25:01 -0600

    feat: Implement grid coloring
    Coloring the grid by clicking or dragging the mouse is now enabled. The
    color input is saved in a variable each time the input changes.

--- resources/js/designs.js
+++ resources/js/designs.js
@@ -1,34 +1,73 @@
 /* jshint esversion: 6 */
-// Select color input
-// Select size input
+// TODO: Cleanup code, Fix comments, research if functions and variables need to be in the ready function or not for best preformance
 
-// When size is submitted by the user, call makeGrid()
+const pixelCanvas = $("#pixel-canvas"), //Cache common DOM lookups
+  sizePicker = $("#size-picker"),
+  colorPicker = $("#color-picker"),
+  inputHeight = $("#input-height"),
+  inputWidth = $("#input-width");
 
-const pixelCanvas = $("#pixel-canvas");
-let htmlGrid = [];
+let mouseDown = false, // Tracks status of mouse button
+  color = colorPicker.val(),     //initialize input values
+  gridHeight = inputHeight.val(),
+  gridWidth = inputWidth.val();
 
+function setGridHandler(){     //function to set event listeners on the new grid
+  const td = $("#pixel-canvas td");
+  td.mouseover(function() {   //color grid while dragging mouse
+    if (mouseDown) {
+      $(this).css({
+        background: color
+      });
+    }
+  });
+  td.click(function() {   //color while clicking a single cell
+    $(this).css({
+      background: color
+    });
+  });
+}
 
 function makeGrid() {
-  pixelCanvas.empty();
-  let gridHeight = $("#input-height").val();
-  let gridWidth = $("#input-width").val();
+  pixelCanvas.empty(); //removes all children from the table
   let htmlGrid = [];
-  for (var i = 0; i < gridHeight; i++) {
-    htmlGrid.push("<tr>");
-    for (var j = 0; j < gridWidth; j++) {
-      htmlGrid.push("<td></td>");
+  //storing the grid html in array to be pushed all at once instead of manipulating the dom in the loops
+  for (let i = 0; i < gridHeight; i++) {
+    htmlGrid.push("<tr>"); //add row to array
+    for (let j = 0; j < gridWidth; j++) {
+      htmlGrid.push("<td></td>"); //add cell
     }
-    htmlGrid.push("</tr>");
+    htmlGrid.push("</tr>"); //close row
   }
-  pixelCanvas.append(htmlGrid);
-  // Your code goes here!
-
+  pixelCanvas.append(htmlGrid); //add the grid markup to the DOM
+  setGridHandler();
 }
-$("#size-picker").submit(function(event){
-  event.preventDefault();
-  makeGrid();
-});
 
 $(function() {
   makeGrid();
+  // When size is submitted by the user, call makeGrid()
+  sizePicker.submit(function(event) {
+    event.preventDefault();
+    gridHeight = inputHeight.val();
+    gridWidth = inputWidth.val();
+    makeGrid();
+  });
+
+  colorPicker.change(function(event) {  //changes color value when input is changed
+    color = colorPicker.val();
+  });
+
+  // I had issues with dragging nonexistant things bugging out the mouseDown value. this code fixes that problem
+  //by dissallowing dragging on the webpage
+  $(document).on('dragstart', function(event) {
+    event.preventDefault();
+  });
+
+  $(document).mousedown(function() {
+      mouseDown = true; // When mouse goes down, set  to true
+    })
+    .on("mouseup", function() {
+      mouseDown = false; // When mouse goes up, set  to false
+    });
+
 });
