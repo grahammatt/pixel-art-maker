1c2cc50483da87846b2ac8cf67278b76933dbe84
grahammatt <grahammatt@live.com>
2018-02-14 15:42:29 -0600

    feat: Implement pen, eraser, and color picker tools
    Tools have been added to the web app. the pen tool is to draw, the
    eraser changes tiles in the grid to white, the picker tool changes the
    active color to a selection on the canvas. The UI is not final, to be
    worked on next.

--- resources/js/designs.js
+++ resources/js/designs.js
@@ -1,68 +1,102 @@
 /* jshint esversion: 6 */
 // TODO: fix code to match styleguide, (ie:const should be all caps) research which functions and variables need to be in the ready function for best preformance
 
-const pixelCanvas = $("#pixel-canvas"), //Cache common DOM lookups
-  sizePicker = $("#size-picker"),
-  colorPicker = $("#color-picker"),
-  inputHeight = $("#input-height"),
-  inputWidth = $("#input-width");
+const PIXEL_CANVAS = $("#pixel-canvas"), //Cache common DOM lookups
+  SIZE_PICKER = $("#size-picker"),
+  COLOR_PICK = $("#color-picker"),
+  HEIGHT = $("#input-height"),
+  WIDTH = $("#input-width");
 
 let mouseDown = false, // Tracks status of mouse button
-  color = colorPicker.val(); //initialize input color values
+  bgDefault = "#ffffff",
+  color = COLOR_PICK.val(); //initialize input color values
+
+function tool(){
+  return $('input[name=tool]:radio:checked').val();
+}
+
+function colorChange(newColor) {
+  color = newColor;
+  $(".color-tile").css({ //change color on the customized picker button
+    background: color
+  });
+}
 
 function setGridHandler() { //function to set event listeners on the new grid
   const td = $("#pixel-canvas td");
+
   td.mouseover(function() { //color grid while dragging mouse
     if (mouseDown) {
-      $(this).css({
-        background: color
-      });
+      switch (tool()) {
+        case "pen":
+          $(this).css({
+            background: color
+          });
+          break;
+        case "eraser":
+          $(this).css({
+            background: bgDefault
+          });
+          break;
+      }
     }
   });
+
   td.click(function() { //color while clicking a single cell
-    $(this).css({
-      background: color
-    });
+    switch (tool()) {
+      case "pen":
+        $(this).css({
+          background: color
+        });
+        break;
+      case "eraser":
+        $(this).css({
+          background: bgDefault
+        });
+        break;
+      case "picker":
+        colorChange($(this).css("backgroundColor"));
+        break;
+    }
   });
 }
 
 function makeGrid() {
-  pixelCanvas.empty(); //removes all children from the table
+  PIXEL_CANVAS.empty(); //removes all children from the table
   //storing the grid html in a document fragment to be pushed all at once instead of manipulating the dom in the loops
   let fragment = document.createDocumentFragment(),
-    gridHeight = inputHeight.val(),
-    gridWidth = inputWidth.val();
+    gridHeight = HEIGHT.val(),
+    gridWidth = WIDTH.val();
 
   for (let i = 0; i < gridHeight; i++) {
     fragment.prepend(document.createElement('tr')); //prepend empty row to fragment
-    let row = fragment.firstElementChild;  //cache created row
+    let row = fragment.firstElementChild; //cache created row
     for (let j = 0; j < gridWidth; j++) {
       row.append(document.createElement('td')); //append cell to cached row
     }
   }
-  pixelCanvas.append(fragment); //add the grid markup to the DOM
+  PIXEL_CANVAS.append(fragment); //add the grid markup to the DOM
   setGridHandler(); //set the event listners on the grid
 }
 
 $(function() {
   makeGrid();
   // When size is submitted by the user, call makeGrid()
-  sizePicker.submit(function(event) {
+  SIZE_PICKER.submit(function(event) {
     event.preventDefault();
     makeGrid();
   });
 
-  colorPicker.change(function(event) { //changes color value when input is changed
-    color = colorPicker.val();
-    $(".color-tile").css({ //change color on the customized picker button
-      background: color
-    });
+  COLOR_PICK.change(function(event) { //changes color value when input is changed
+    colorChange(COLOR_PICK.val());
   });
 
   // TODO: this works for now but when I add more features i may need to cahe the lookups or change white to transparent
-  $("#clear-grid").click(function(){
-    if (confirm("This will erase all your work!\nAre you sure you want to erase the canvas?")){
-        $('td').css({background: "#ffffff"});
+  $("#clear-grid").click(function() {
+    if (confirm("This will erase all your work!\nAre you sure you want to erase the canvas?")) {
+      $('td').css({
+        background: bgDefault
+      });
     }
   });
 
